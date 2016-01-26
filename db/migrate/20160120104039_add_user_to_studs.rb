fb53fc94cdd49f3bc51411e9d202f66375fced9d
uzaif313 <uzaif313@gmail.com>
2016-01-26 14:03:12 +0530

    Modify the migration

--- db/migrate/20160120104039_add_user_to_studs.rb
+++ db/migrate/20160120104039_add_user_to_studs.rb
@@ -1,5 +1,5 @@
 class AddUserToStuds < ActiveRecord::Migration
   def change
-     add_reference :stud, :user, index: true, foreign_key: true
+     add_reference :studs, :user, index: true, foreign_key: true
   end
 end
