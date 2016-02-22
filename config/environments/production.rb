a2b6238b60efa256e2cc270e2b745cf5a9df4a6e
uzaif313 <uzaif313@gmail.com>
2016-02-22 12:22:12 +0530

    Add Serve static file in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.serve_static_files = true 
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
